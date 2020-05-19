1770ad1b6934d0d5016fe32c78c40fd649807627
agutierrezd <agutierrezd@mincit.gov.co>
2020-05-19 06:14:30 -0500

    Common Pages

--- include/appsettings.php
+++ include/appsettings.php
@@ -644,9 +644,9 @@ $suggestAllContent = true;
 $strLastSQL = "";
 $showCustomMarkerOnPrint = false;
 
-$projectBuildKey = "261_1589855724";
+$projectBuildKey = "263_1589855724";
 $wizardBuildKey = "34656";
-$projectBuildNumber = "261";
+$projectBuildNumber = "263";
 
 $mlang_messages = array();
 $mlang_charsets = array();
